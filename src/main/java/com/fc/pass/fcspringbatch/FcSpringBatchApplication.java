cd2653e3c51bc580673ba298dcdb251e5d7f7910
jeongho <jeongholee0918@gmail.com>
2024-05-01 15:41:29 +0900

    Initial commit

---
+++ src/main/java/com/fc/pass/fcspringbatch/FcSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.fc.pass.fcspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcSpringBatchApplication.class, args);
+    }
+
+}
