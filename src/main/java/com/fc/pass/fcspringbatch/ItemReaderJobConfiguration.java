308bb19c7a13d46c223384e3807ccbac4429464c
jeongho <jeongholee0918@gmail.com>
2024-05-03 15:20:36 +0900

    itemReader : 파일 읽어오기

---
+++ src/main/java/com/fc/pass/fcspringbatch/ItemReaderJobConfiguration.java
@@ -0,0 +1,82 @@
+package com.fc.pass.fcspringbatch;
+
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class ItemReaderJobConfiguration {
+
+    @Bean
+    public Job job(JobRepository jobRepository, Step step){
+        return new JobBuilder("itemReaderJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step step(
+            JobRepository jobRepository,
+            PlatformTransactionManager transactionManager,
+            ItemReader<User> jsonItemReader
+    ){
+        return new StepBuilder("step", jobRepository)
+                .<User,User>chunk(2, transactionManager)
+                .reader(jsonItemReader)
+                .writer(System.out::println)
+                .build();
+
+    }
+
+
+    @Bean
+    public FlatFileItemReader<User> flatFileItemReader(){
+        return new FlatFileItemReaderBuilder<User>()
+                .name("flatFileItemReader")
+                .resource(new ClassPathResource("users.txt"))
+                .linesToSkip(2) // 초반 2줄은 스킵.
+                .delimited().delimiter(",") // 구분자를 결정.
+                .names("name", "age", "region", "telephone")
+                .targetType(User.class)
+//                .strict(false) // 해당 파일이 없어도 넘어가도 괜찮다면 이 옵션을 줘도 됨.
+                .build();
+    }
+
+    @Bean FlatFileItemReader<User> fixedLengthFlatFileItemReader(){
+        return new FlatFileItemReaderBuilder<User>()
+                .name("fixedLengthFlatFileItemReader")
+                .resource(new ClassPathResource("usersFixedLength.txt"))
+                .linesToSkip(2) // 초반 2줄은 스킵.
+                . fixedLength()
+                .columns(new Range[]{new Range(1,2), new Range(3,4), new Range(5,6), new Range(7,19)})
+                .names("name", "age", "region", "telephone")
+                .targetType(User.class)
+//                .strict(false) // 해당 파일이 없어도 넘어가도 괜찮다면 이 옵션을 줘도 됨.
+                .build();
+    }
+
+    @Bean
+    public JsonItemReader<User> jsonItemReader(){
+        return new JsonItemReaderBuilder<User>()
+                .name("jsonItemReader")
+                .resource(new ClassPathResource("users.json"))
+                .jsonObjectReader(new JacksonJsonObjectReader<>(User.class))
+                .build();
+    }
+}
---
+++ src/main/java/com/fc/pass/fcspringbatch/User.java
@@ -0,0 +1,12 @@
+package com.fc.pass.fcspringbatch;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+    private String age;
+    private String region;
+    private String telephone;
+}
